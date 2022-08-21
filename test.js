c67075c32146a7be942a51e7cd94b277b523217e
leahmarke <lmarke@live.com>
2022-08-21 18:32:33 -0400

    from class

--- test.js
+++ test.js
@@ -3,74 +3,25 @@ const axios = require("axios").default;
 const assert = require("assert");
 let contactArray;
 const getAddress = () => {
-  axios
-    .get("https://randomuser.me/api/?results=2")
-    .then((res) => {
-      if (!res.ok) {
-        throw Error(res.statusText);
-      }
-      return res.json();
-    })
-    .then((address) => {
-      contactArray = address.results;
-    });
+  axios.get("https://randomuser.me/api/?results=1").then(function (response) {
+    // handle success
+    // console.log(response.data.results);
+    contactArray = response.data.results;
+    console.log("this is my contact Array:", contactArray);
+    return contactArray;
+  });
 };
+console.log(response.data.results.name.first);
+
 if (typeof describe === "function") {
-  describe("getAddress()", () => {
-    it("should return 5 results", () => {
+  describe("getAddress()", function () {
+    it("should return 2 results", function () {
       getAddress();
-      assert.equal(address.results.length, 2);
+      assert.equal(contactArray.length, 1);
     });
-    it("should be able to give error if not ok", () => {
+    it("should be able to give error if not ok", function () {
       getAddress();
-      assert.equal(address.results.length > 2);
+      assert.equal(contactArray.length > 2);
     });
-    // it("should be able to test if the wrong IP address is entered", () => {
-    //   address();
-    //   assert.equal(, "You guessed it!");
-    // });
-
-    // it("should be able to identify typeof is a string", () => {
-    //   getAddress();
-    //   assert.equal(address.results.firstName.typeof, string);
-    // });
-
-    // it("should be able to detect a win", () => {
-    //   assert.equal(mastermind(solution), "You guessed it!");
-    // });
-
-    // it("should be able to detect a win", () => {
-    //   assert.equal(mastermind(solution), "You guessed it!");
-    // });
   });
 }
-// } else {
-//   getPrompt();
-// }
-
-//     .then(function (response) {
-//       // handle success
-//       console.log(response);
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       console.log(error);
-//     })
-//     .then(function () {
-//       // always executed
-//     });
-// };
-
-// let contactArray;
-
-// const getAddress = () => {
-//   fetch("https://randomuser.me/api/?results=5")
-//     .then((res) => {
-//       if (!res.ok) {
-//         throw Error(res.statusText);
-//       }
-//       return res.json();
-//     })
-//     .then((address) => {
-//       contactArray = address.results;
-//     }}
